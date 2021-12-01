f6776961c1ed115ce5455cd25af4b1eee33b452a
Md. Tarek Hasan <mhasan181076@bscse.uiu.ac.bd>
2021-12-01 22:28:58 +0600

    Encoding Function Added

--- Huffman_Coding.cpp
+++ Huffman_Coding.cpp
@@ -1,15 +1,55 @@
 #include<bits/stdc++.h>
 using namespace std;
+struct TreeList
+{
+    int frequency;
+    char c;
+    TreeList* left;
+    TreeList* right;
+};
+
+typedef struct TreeList TreeList;
+
+TreeList* encoding(string &str)
+{
+    auto cmp = [](TreeList* a, TreeList* b)
+    {
+        return a->frequency>b->frequency;
+    };
+
+    priority_queue< TreeList* , vector< TreeList* >, decltype(cmp)> pq(cmp);
+    map< char, int > mp;
+    for(char c : str)
+        mp[c]++;
+    for(auto x : mp)
+    {
+        TreeList* temp = new TreeList();
+        temp->frequency = x.second;
+        temp->c = x.first;
+        temp->left = temp->right = NULL;
+        pq.push(temp);
+    }
+    while(pq.size()>1)
+    {
+        TreeList* temp = new TreeList();
+
+        temp->left = pq.top();
+        pq.pop();
+        temp->right = pq.top();
+        pq.pop();
+        temp->c = '#';
+        temp->frequency = temp->left->frequency+temp->right->frequency;
+        pq.push(temp);
+    }
+    return pq.top();
+}
+
 int main()
 {
     #ifdef TarekHasan
         freopen("input.txt","r",stdin);
     #endif // TarekHasan
     string str; cin >> str;
-    map< char, int > mp;
-    for(char c : str)
-        mp[c]++;
-    for(auto x : mp)
-        cout << x.first << " " << x.second << "\n";
+    encoding(str);
     return 0;
 }
